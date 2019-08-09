a16640e697ed38c9285298f9efeac37a967b87c0
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-08-09 08:46:56 +0530

    Import page moved to components/page

--- app/Routes.js
+++ app/Routes.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import routes from './constants/routes';
-import App from './containers/App';
-import HomePage from './containers/HomePage';
-import ExportPage from './containers/ExportPage';
+import { App, ExportPage, HomePage, ImportPage } from './containers';
 
 export default () => (
   <App>
     <Switch>
       <Route path={routes.EXPORT} component={ExportPage} />
+      <Route path={routes.IMPORT} component={ImportPage} />
       <Route path={routes.HOME} component={HomePage} />
     </Switch>
   </App>
--- app/components/page/index.js
+++ app/components/page/index.js
@@ -1,2 +1,3 @@
 export { default as Home } from './Home';
 export { default as Export } from './Export';
+export { default as Import } from './Import';
